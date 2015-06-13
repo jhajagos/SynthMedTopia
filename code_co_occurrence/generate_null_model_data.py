c4bb814aece4007c322e4e3fe71ad70a1b4e8bde
jhajagos <risk.limits@gmail.com>
2015-06-13 08:22:42 -0400

    Started adding files for generating diagnostic data

---
+++ code_co_occurrence/generate_null_model_data.py
@@ -0,0 +1,34 @@
+__author__ = 'jhajagos'
+
+
+"""
+    Generate a CSV file for loading the file into a PostGres database
+
+        Two axes in age and gender
+        A code list (code, description)
+
+        Number of patients
+        Each encounter gets a number
+
+    The data should fit a null model that would be an unrealistic representation
+        (e.g., a 2 year old male with a pregnancy)
+
+        Here the null model assumes that a diagnosis on any given date within the period
+        Any code could be generated
+
+
+
+"""
+
+
+def get_acs_files():
+    pass
+
+def load_dimensions():
+    pass
+
+def load_codes():
+    pass
+
+def main(number_of_days, number_of_patients, number_of_encounters):
+    pass
