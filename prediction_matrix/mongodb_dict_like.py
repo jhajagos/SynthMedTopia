a112f50b5363fedecf0bbd85bc1f5ea46e4bfb10
Janos G. Hajagos <risk.limits@gmail.com>
2015-11-24 22:31:45 -0500

    Added class and tests for creating a dict like object backed by mongodb

--- prediction_matrix/mongodb_dict_like.py
+++ prediction_matrix/mongodb_dict_like.py
@@ -15,12 +15,26 @@ class DictMappingMongo(object):
         self.collection_name = collection_name
         self.name_to_use_as_key = name_to_use_as_key
 
+        self.db = self.mongo_client_obj[self.database_name]
+        self.collection = self.db[self.collection_name]
+
     def __getitem__(self, key):
-        pass
+        item_dict = self.collection.find_one({self.name_to_use_as_key: key})
+        if item_dict is not None:
+            return item_dict
+        else:
+            raise KeyError
 
     def __setitem__(self, key, val):
-        pass
+        if type(val) == type({}):
+            val[self.name_to_use_as_key] = key
+            if self.collection.find_one({self.name_to_use_as_key: key}):
+                self.collection.replace_one({self.name_to_use_as_key: key}, val)
+            else:
+                self.collection.insert(val)
+        else:
+            raise TypeError
 
     def __len__(self):
-        pass
+        return self.collection.count()
 
---
+++ prediction_matrix/test_mongo_db_dict_like.py
@@ -0,0 +1,56 @@
+__author__ = 'janos'
+
+import unittest
+
+from mongodb_dict_like import DictMappingMongo
+from pymongo import MongoClient
+
+class TestDictMapping(unittest.TestCase):
+
+    def setUp(self):
+        self.client = MongoClient()
+        self.collection = "unittest"
+        self.database = "testing_py"
+        self.field_name = "transaction_id"
+
+        self.client[self.database][self.collection].remove()
+
+    def test_class(self):
+        test_collection = DictMappingMongo(self.client, self.database, self.collection, self.field_name)
+        n = len(test_collection)
+        self.assertEqual(n, 0)
+
+        test_collection["12345"] = {"name": "hello"}
+
+        n1 = len(test_collection)
+        self.assertEqual(n1, 1)
+
+        item_dict = test_collection["12345"]
+        self.assertIsNotNone(item_dict)
+
+        test_collection["12345"] = {"name": "sparkle"}
+
+        n2 = len(test_collection)
+        self.assertEqual(n2, 1)
+
+        try:
+            test_collection["123"]
+        except KeyError:
+            self.assertTrue(1)
+
+        test_collection["54321"] = {"name": "twinkle"}
+
+        n3 = len(test_collection)
+        self.assertEqual(n3, 2)
+
+        try:
+            test_collection["66666"] = 4
+        except TypeError:
+
+            self.assertTrue(1)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
